41780d6744b2a9b1c87c0b71ea1696e9ac863eb7
patel shruti <ptlshruti2692004@gmail.com>
2026-02-04 12:54:53 +0530

    third commit

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,10 +1,13 @@
 import express from "express";
-import { create, getAllUsers, getUserById } from "../controller/userController.js";
+import { create, getAllUsers, getUserById, update } from "../controller/userController.js";
 
 const route = express.Router();
 
 route.post("/users", create);
 route.get("/users", getAllUsers);
 route.get("/users/:id", getUserById);
+route.put("/users/:id", update);   
 
 export default route;
+
+
